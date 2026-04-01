4c34a82d399de902fd7c48ae9ce67a248d4723c1
CedricMeeuwis <cedric.meeuwis@hotmail.com>
2026-04-01 09:40:20 +0200

    style changes + step in practical

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,20 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  infoBlock = `<span class="fw-bold">Info</span>
+  infoBlock = `<h4>Info</h4>
         <a href="/" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Home</a>
         <a href="over-mij" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Over mij</a>
         <a href="contact" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Contact</a>
         <a href="praktisch" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Praktisch</a>`;
-  offerBlock = `<span class="fw-bold">Behandelingen</span>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Stotteren</a>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Preverbale logopedie</a>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Spraakontwikkeling en afwijkende mondgewoonten</a>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Taalontwikkeling en taalstoornis</a>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Leerstoornissen en leerproblemen</a>
-        <a href="behandelingen" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">IMPACT-training</a>`;
-  contactBlock = `<span class="fw-bold">Contact</span>
+  offerBlock = `<h4>Behandelingen</h4>
+        <a href="behandelingen/info/1" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Stotteren</a>
+        <a href="behandelingen/info/2" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Preverbale logopedie</a>
+        <a href="behandelingen/info/3" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Spraakontwikkeling en afwijkende mondgewoonten</a>
+        <a href="behandelingen/info/4" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Taalontwikkeling en taalstoornis</a>
+        <a href="behandelingen/info/5" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Leerstoornissen en leerproblemen</a>
+        <a href="behandelingen/info/6" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">IMPACT-training</a>`;
+  contactBlock = `<h4>Contact</h4>
         <a href="https://maps.google.com/?q=Bergen+38" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">Bergen 38,<br/>2370 Arendonk</a>
         <a href="mailto:logopedie.eliensegers@gmail.com" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover text-break">logopedie.eliensegers@gmail.com</a>
-        <a href="tel:+32445526390" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">+32 44 552 63 90</a>`;
+        <a href="tel:+32470710392" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-dark link-primary-hover">+32 470 71 03 92</a>`;
 }
